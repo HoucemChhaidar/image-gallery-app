8d3da51f80f16665c1d53f85fb5e0d98d24f2be6
HoucemChhaidar <houcem.chhaidar@gmail.com>
2024-06-05 10:17:48 +0100

    Fixed scrolling behaviour on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,58 +20,67 @@ class HomeScreen extends StatelessWidget {
             top: 48.0,
             right: 20.0,
           ),
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Header(),
-                SizedBox(
-                  height: 24.0,
-                ),
-                CustomSearchBar(),
-                SizedBox(
-                  height: 24.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Tags',
-                      style: darkMediumHeading,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'See All',
-                          style: TextStyle(
-                            fontFamily: 'SFProDisplay',
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.w500,
-                            color: primaryColors[600],
-                          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Header(),
+              SizedBox(
+                height: 24.0,
+              ),
+              CustomSearchBar(),
+              SizedBox(
+                height: 24.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Tags',
+                    style: darkMediumHeading,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'See All',
+                        style: TextStyle(
+                          fontFamily: 'SFProDisplay',
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w500,
+                          color: primaryColors[600],
                         ),
-                        SizedBox(
-                          width: 4.0,
-                        ),
-                        SvgPicture.asset(
-                          'assets/icons/chevron_right.svg',
-                        ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      SizedBox(
+                        width: 4.0,
+                      ),
+                      SvgPicture.asset(
+                        'assets/icons/chevron_right.svg',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12.0,
+              ),
+              TagsList(),
+              SizedBox(
+                height: 12.0,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 12.0,
+                      ),
+                      CardsTilesList(),
+                    ],
+                  ),
                 ),
-                SizedBox(
-                  height: 12.0,
-                ),
-                TagsList(),
-                SizedBox(
-                  height: 24.0,
-                ),
-                CardsTilesList(),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
