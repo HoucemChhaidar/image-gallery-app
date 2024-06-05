28cdab41508630069601f687e6bd1ce16ed12348
HoucemChhaidar <houcem.chhaidar@gmail.com>
2024-06-05 10:16:44 +0100

    Removed default top padding from the GridView widget

--- lib/components/cards_tiles_list.dart
+++ lib/components/cards_tiles_list.dart
@@ -7,26 +7,30 @@ class CardsTilesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      physics: BouncingScrollPhysics(),
-      shrinkWrap: true,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        mainAxisExtent: 256.0,
-        crossAxisCount: 2,
-        crossAxisSpacing: 16.0,
-        mainAxisSpacing: 16.0,
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      child: GridView.builder(
+        physics: BouncingScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisExtent: 256.0,
+          crossAxisCount: 2,
+          crossAxisSpacing: 16.0,
+          mainAxisSpacing: 16.0,
+        ),
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return CardTile(
+            imageIndex: index + 1,
+            iconSize: 12.0,
+            iconPadding: 8.0,
+            authorSize: 24.0,
+            aboutAuthorPadding: 8.0,
+            authorStyle: smallAuthorName,
+          );
+        },
       ),
-      itemCount: 4,
-      itemBuilder: (context, index) {
-        return CardTile(
-          imageIndex: index + 1,
-          iconSize: 12.0,
-          iconPadding: 8.0,
-          authorSize: 24.0,
-          aboutAuthorPadding: 8.0,
-          authorStyle: smallAuthorName,
-        );
-      },
     );
   }
 }
